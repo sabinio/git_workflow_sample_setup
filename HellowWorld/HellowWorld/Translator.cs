eb5c90ae1a9545d68fe4105d978ecdb1ed7ec927
james.duggan <james.duggan@sabin.io>
2017-03-27 22:19:31 +0100

    Commit 2 : branch

---
+++ HellowWorld/HellowWorld/Translator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HellowWorld
+{
+    static class Translator
+    {
+        public static string SayHello()
+        {
+            return "Hello World";
+        }
+    }
+}
