fa6da61bdf852a97b9082c1c5ee6a9f8cab3c0b7
james.duggan <james.duggan@sabin.io>
2017-03-27 22:24:51 +0100

    Commit 4 : branch

--- HellowWorld/HellowWorld/Program.cs
+++ HellowWorld/HellowWorld/Program.cs
@@ -11,6 +11,7 @@ namespace HellowWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("{0} : commit 4 branch", Translator.SayHello());
         }
     }
 }
